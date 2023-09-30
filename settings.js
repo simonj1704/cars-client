01d5d8800938c5b16fc74442c761b4261a6a9d06
simonj1704 <simo915m@stud.kea.dk>
2023-09-30 22:28:11 +0200

    Fixed url

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://dat3week2simon.azurewebsites.net/api"
+export const API_URL = "https://dat3week2simon.azurewebsites.net/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"   
